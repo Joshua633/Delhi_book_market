f3da36ab19eee9885b3493bc37ba7d197da1fa8f
Joshua Deepu <85779222+Joshua633@users.noreply.github.com>
2025-09-11 15:33:29 +0300

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import 'react-native-get-random-values'
 
-const supabaseUrl = 'https://tslkxbsdjyyzisygcalp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzbGt4YnNkanl5emlzeWdjYWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDMyOTcsImV4cCI6MjA3MjkxOTI5N30.MGyB4oB37VxPVKUh84v5EmfeerVcab5RMhG49UPxLhs'
+const supabaseUrl = 'https://url.supabase.co'
+const supabaseAnonKey = 'anon_key'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
